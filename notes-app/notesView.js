f5339c779b4be8099ab24256aa184261367aab79
josh-p-git <joshua.penney91@gmail.com>
2023-02-03 14:39:54 +0000

    notes app updated with add note

--- notes-app/notesView.js
+++ notes-app/notesView.js
@@ -2,6 +2,18 @@ class NotesView {
     constructor(model) {
         this.model = model;
         this.mainContainerEl = document.querySelector('#main-container');
+        this.buttonEl = document.querySelector('#add-message-button');
+        this.inputEl = document.querySelector('#message-input');
+
+        this.buttonEl.addEventListener('click', () => {
+            const newNote = this.inputEl.value;
+            this.addNewNote(newNote);
+        });
+    }
+
+    addNewNote(newNote) {
+        this.model.addNote(newNote);
+        this.displayNotes();
     }
 
     displayNotes() {
--- notes-app/notesView.test.js
+++ notes-app/notesView.test.js
@@ -11,16 +11,29 @@ describe('Notes view', () => {
   it('displays two notes', () => {
     document.body.innerHTML = fs.readFileSync('./index.html');
 
-    // 1. Setting up model and view
     const model = new NotesModel();
     const view = new NotesView(model);
     model.addNote('A first note');
     model.addNote('Another one');
     
-    // 2. Display the notes on the page
     view.displayNotes();
 
-    // 3. There should now be 2 div.note on the page
     expect(document.querySelectorAll('div.note').length).toEqual(2);
+  });
+
+  it('adds a new note', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+  
+    const model = new NotesModel();
+    const view = new NotesView(model);
+  
+    const input = document.querySelector('#message-input');
+    input.value = 'My test message';
+  
+    const button = document.querySelector('#add-message-button');
+    button.click();
+  
+    expect(document.querySelectorAll('div.note').length).toEqual(1);
+    expect(document.querySelectorAll('div.note')[0].textContent).toEqual('My test message');
   });
 });
